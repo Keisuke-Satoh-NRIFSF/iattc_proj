236caf08854ae08dc6b628ac752624a0622bc2c5
Keisuke-Satoh-NRIFSF <38120631+Keisuke-Satoh-NRIFSF@users.noreply.github.com>
2019-02-14 11:40:11 -0800

    added new file

---
+++ test.R
@@ -0,0 +1,6 @@
+library(devtools)
+#install_github('hoyles/cpue.rfmo')
+install_github('hoyles/cpue.rfmo', usename="Keisuke-Satoh-NRIFSF", auth_token = "d7731bbb55cf03ab45738e3e91f15d25fdeb04f1")
+library(cpue.rfmo)
+
+plot(1:10)
